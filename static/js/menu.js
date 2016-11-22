9661063f89563bfaea755e93ad3d89629e4419ec
Adam Tindale <adam@adamtindale.com>
2016-11-21 21:55:22 -0500

    basic menu working. style needs work

--- static/js/menu.js
+++ static/js/menu.js
@@ -7,6 +7,8 @@ var menumenu = function(){
         bloglinks[i].hidden = true;
     }
 
+    var blogcategories = document.querySelectorAll('#menu-categories > li');
+    
     document.getElementById("menu-categories").hidden = true;
 
     document.getElementById('blogtitle').querySelector('a').onclick = function(e){
@@ -22,4 +24,31 @@ var menumenu = function(){
         // reset the link 
         this.onclick = function(e){};
     };
+
+    var tada =  document.querySelectorAll('.menu-category > a') ;
+    for (var i = 0; i < tada.length; i++){
+        tada[i].onclick = function(index){
+            return function (e) {
+                //console.log(index);
+                e.preventDefault();
+                e.stopPropagation();
+
+                var mybloglinks = blogcategories[index].querySelectorAll('ul > li');
+                //console.log(mybloglinks);
+
+                for ( var j = 0; j < mybloglinks.length; j++){
+                    mybloglinks[j].hidden = false;
+                }
+
+                for ( var k = 0; k < blogcategories.length; k++){
+                    if (k !== index) {
+                        blogcategories[k].hidden = true;
+                    }else{
+                        blogcategories[k].hidden = false;
+                    }
+                }
+                this.onclick = function(e){};
+            };
+        }(i);
+    }
 };
